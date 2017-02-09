9e6201489c54682ce204ef67ee6b783d04f1e077
Chris Coralli <ccoralli@gmail.com>
2017-02-09 14:15:46 -0500

    Products populated from array using jQuery.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// actual image: etsyData.results[i].Images[0].url_75x75
+// image: etsyData.results[i].Images[0].url_75x75
 //
 // shop name: etsyData.results[i].Shop.login_name
 //
@@ -9,3 +9,16 @@
 // description(hover): etsyData.results[i].description
 //
 // link: etsyData.results[i].url
+
+var populateProducts = function ()  {
+  for (i = 0; i < 16; i++)  {
+    var image = etsyData.results[i].Images[0].url_170x135;
+    var shopName = etsyData.results[i].Shop.login_name;
+    var price = etsyData.results[i].price;
+    var title = etsyData.results[i].title.substring(0, 24);
+    var product = $(".product").eq(i);
+    $(product).html(`<img class="image" src="${image}" /> \n <div class="title">${title}...</div> \n <div class="shop-name">${shopName}</div> \n <div class="price">${price}</div>`);
+  }
+}
+
+populateProducts();
