8b263816c051a5ff439600767344ad8fb5914f63
Chris Coralli <ccoralli@gmail.com>
2017-02-09 12:22:12 -0500

    inital commit, html framework + needed array objects logged.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// actual image: etsyData.results[i].Images[0].url_75x75
+//
+// shop name: etsyData.results[i].Shop.login_name
+//
+// price: etsyData.results[i].price
+//
+// title: etsyData.results[i].title
+//
+// description(hover): etsyData.results[i].description
+//
+// link: etsyData.results[i].url
